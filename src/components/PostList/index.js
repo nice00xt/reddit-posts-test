742c2a8e62754ccd07b94bfcc29324138411d935
Juan <juan00xt@gmail.com>
2021-05-13 19:49:41 -0500

    Add loading transition

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -2,26 +2,25 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPost } from "../../actions";
 import PostItem from '../PostItem';
+import Spinner from '../Spinner';
 
 export const PostList = () => {
   const dispatch = useDispatch();
   const { list, isLoading } = useSelector(({ posts }) => posts);
   useEffect(() => dispatch(fetchPost()), [dispatch]);
 
-  if (isLoading) {
-    return <span>Loading...</span>
-  }
-
   return (
     <div className='post-sidebar'>
-      <div className='post-content'>
-        <div className='post-content__header'>
-          <h3 className='post-content__title'>Reddit Posts</h3>
+      {isLoading ? <Spinner /> : (        
+        <div className='post-content fade-in--top'>
+          <div className='post-content__header'>
+            <h3 className='post-content__title'>Reddit Posts</h3>
+          </div>
+          <ul className='post-content__list'>
+            <PostItem list={list}/>
+          </ul>
         </div>
-        <ul className='post-content__list'>
-          <PostItem list={list}/>
-        </ul>
-      </div>
+      )}
     </div>
   );
 };
